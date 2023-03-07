d285312bdc279bed1b08f911181d267319d418c2
acfidelino <acfidelino@student.apc.edu.ph>
2023-03-07 21:30:02 +0800

    Error Handling II

--- db/mysql_insert.php
+++ db/mysql_insert.php
@@ -1,7 +1,7 @@
 <?php
 
 // include_once "db-apcwebprog.php";
-include_once "../../lab3/ci4/app/Views/pages/db-localhost.php";
+include_once "../../lab3/ci4/app/Views/pages/db-apcwebprog.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
@@ -13,9 +13,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           VALUES ('$name', '$email', '$gender', '$comment', '$website')";
 
   if ($conn->query($sql) === TRUE) {
-    header('Location: http://localhost/lab3/ci4/public/guest?query=success');
+    header('Location: http://apcwebprog.csf.ph/~acfidelino/lab3/ci4/public/guest?query=success');
   } else {
-    header('Location: http://localhost/lab3/ci4/public/guest?query=fail');
+    header('Location: http://apcwebprog.csf.ph/~acfidelino/lab3/ci4/public/guest?query=fail');
   }
 
   $conn->close();
