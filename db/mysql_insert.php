37a67166c9cc9c8f48fc9b5ecb49aff6eba81af9
acfidelino <acfidelino@student.apc.edu.ph>
2023-03-07 21:55:06 +0800

    Debugging...

--- db/mysql_insert.php
+++ db/mysql_insert.php
@@ -1,7 +1,7 @@
 <?php
 
 // include_once "db-apcwebprog.php";
-include_once "../lab3/ci4/app/Views/pages/db-apcwebprog.php";
+include_once "db-apcwebprog.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
